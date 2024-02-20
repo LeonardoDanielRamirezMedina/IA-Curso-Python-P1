a4a89ccab59fc7429e3625cdb6daa63b1fbdc79f
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-19 23:43:48 -0600

    Tema 4

---
+++ 02_Strings_Concatenar strings.py
@@ -0,0 +1,10 @@
+#Leonardo Daniel Ramirez Medina - 21310138
+#6E - IA
+#19/02/24
+
+palabra1= "Hola"            #Se define string1
+palabra2= "soy Leonardo"    #Se define neustra segundo datos string
+frase= palabra1 + ' ' + palabra2 #Se concatenan guardandose en otra variable
+print(frase)                #se imprimen las variables concatenadas
+
+print("Hola " + "Mundo")    #se concatenan strings sin utilizar variables
