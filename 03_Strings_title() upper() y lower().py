350eab9ad949a6b75e4eb4066e2582c4092e996d
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-20 00:03:34 -0600

    Tema 5

---
+++ 03_Strings_title() upper() y lower().py
@@ -0,0 +1,15 @@
+#Leonardo Daniel Ramírez Medina - 21310138
+#6E - IA
+#19/02/24
+
+texto1= "texto de prueba"
+texto1= texto1.title()      #Manera 1 para escribir titulos, con cada primera letra de cada palabra en mayúscyulas
+print(texto1)
+
+texto2="Esto es un titulo"
+print(texto2.title())       #Manera 2 para usar la función title()
+
+texto3="BuEnas TArdEs"
+print(texto3.upper())       #Se usa upper() para que el contenido de texto3 esté en mayúscula
+print(texto3.lower())       #Se usa lower() para que el contenido de texto3 esté en minúscula
+
