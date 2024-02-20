8d706e329faa71b2892462211c4052208c940aa5
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-19 23:43:24 -0600

    Tema 1-3

---
+++ 01_Variables_Int y Strings.py
@@ -0,0 +1,10 @@
+#Leonardo Daniel Ramírez Medina
+#6E - IA
+#19/02/24
+
+palabra1= "Hola Mundo"      #Se declara la variable palabra y se le asigna un valor
+palabra1= 14                #Se le cambia el valor, asi como el tipo de dato de la variable
+print(palabra1)
+
+palabra2="Palabara con 'comillas simples'" #De esta manera se puede mostrar un mensaje con comillas
+print(palabra2)
