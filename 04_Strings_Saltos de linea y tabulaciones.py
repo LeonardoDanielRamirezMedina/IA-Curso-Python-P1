91fe793b2a51c6cbebbf8911b5f139f0ab42fdab
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-26 23:52:46 -0600

    Tema 6

---
+++ 04_Strings_Saltos de linea y tabulaciones.py
@@ -0,0 +1,10 @@
+#Leonardo Daniel Ramírez Medina
+#6E1 - IA
+#26/02/24
+
+#En este programa se mostrará como utilizar saltos de linea y tabulaciones
+
+texto="Lista de compras:\n2 Manzanas\n4 Plátanos\n1 Lechuga\n7 Gitomates\n"#Se pueden utilizar saltos de linea dentro de varibles tipo string escribiendo "\n"
+print("Lista de compras:\n2 Manzanas\n4 Plátanos\n1 Lechuga\n7 Gitomates\n")#Se utiliza el salto de linea directamente por medio del print añadiendo "\n"
+print(texto)    #Se imrpime lka variabkle guardada previamente, la cual contiene saltos de linea
+print("Lista de compras:\n\t-2 Manzanas.\n\t-4 Plátanos.\n\t-1 Lechuga.\n\t-7 Gitomates.\n")#Se utilizan saltos de linea "\n" ademas de tabulaciones al agregar "\t"
