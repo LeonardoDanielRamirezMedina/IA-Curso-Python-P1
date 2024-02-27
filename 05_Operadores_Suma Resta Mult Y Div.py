e005ae9f2c26713048406f07404eb85f6bca669e
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-27 00:26:45 -0600

    Tema 7

---
+++ 05_Operadores_Suma Resta Mult Y Div.py
@@ -0,0 +1,18 @@
+#Leonardo Daniel Ramírez Medina
+#6E1 - IA
+#26/02/24
+
+#Se hablará de los operadores suma, resta, multiplicación y división
+suma=10+2+8         #Se utiliza el operador suma "+" guardando el resultado en una variable
+resta=34-6-2        #Se utiliza el operador resta "-" y se guarda el resultado en la variable
+mult=2*9*4          #Se utiliza el operador multiplicación "*" guardando el resultado
+div=25/4/1          #Se utiliza el operador división "/" guardando el resultado
+
+print("Suma: ", suma , "\nResta: ", resta, "\nMultiplicación: ",mult, "\nDivisión: ",div) #Se concatenan variables tipo int y string por medio del uso de la coma, ademas de mostrar los resultados
+
+operacion=5+3*3/4  #Se utilizan diferentes operadores en una operación
+print("\nOperación: ",operacion)    #Se muestra el resultado de la operación con diferentes operadores
+
+operacion2=(3+3)*2/2 #Se da prioridad a la operacion suma por medio del uso de parentesis
+print("\nOperación 2: ",operacion2) #se muestra el resultado
+
