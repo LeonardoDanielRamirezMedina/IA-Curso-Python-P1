1ccab7d611b3258c9a6e6f2fb385bdeb7298bf1d
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-27 00:38:08 -0600

    Tema 8

---
+++ 06_Operadores_Exponentes.py
@@ -0,0 +1,8 @@
+#Leonardo Daniel Ramírez Medina
+#6E1 - IA
+#27/02/24
+
+#En este código se explicará como calcular exponentes
+
+exponente=2**5      #Se eleva el numero 2 a la potencia 5 utilizando el doble asterisco "**"
+print("Resultado exponente:",exponente) #Se muestra el resultado de la operación
