c4153f153091c0ff530b377cd7b89230a1d8a393
LeonardoDanielRamirezMedina <160379867+LeonardoDanielRamirezMedina@users.noreply.github.com>
2024-02-27 00:51:15 -0600

    Tema 9

---
+++ 07_Floats_metodo round.py
@@ -0,0 +1,10 @@
+#Leonardo Daniel Ramírez Medina
+#6E1 - IA
+#27/02/24
+
+#En este codigo se explicara el uso del método round() aplicado en datos tipo float
+
+num1 = 23.345567        #Se guarda un dato tipo float con 6 decimales en la variable num1
+num2 = 12.875678        #Se guarda un dato tipo float con 6 decimales en la variable num2
+resultado = num1+num2   #Se guarda el resultado de la operación entre 2 datos tipo float
+print("Resultado: ",round(resultado,3))       #se imprime el resultado utilizando el metodo round() al cual le especificamos que el resultado se le aplique un  redondeo a 3 decimales
